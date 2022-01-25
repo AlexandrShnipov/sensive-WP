6fe599a2e627055c1a160a354e1481938f220bba
AlexandrShnipov <sshnipov@gmail.com>
2022-01-25 18:46:46 +0300

    added :hover for <a>

--- archive.php
+++ archive.php
@@ -63,7 +63,7 @@
                   <div class="thumb">
                     <ul class="thumb-info">
                       <li><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><i class="ti-user"></i><?php the_author(); ?></a></li>
-                      <li><a href="#"><i class="ti-themify-favicon"></i><?php comments_number() ?></a></li>
+                      <li><a href="<?php echo get_the_permalink() ?>"><i class="ti-themify-favicon"></i><?php comments_number() ?></a></li>
                     </ul>
                   </div>
                   <div class="details mt-20">
